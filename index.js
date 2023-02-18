911e8c34249695e1fc20d94438ae47bdc1391eb0
lordoleksiy <lordoleksiy@gmail.com>
2023-02-18 17:56:11 +0200

    adding first request

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer(function (request, response){
-    if(request.url === "/jokes" && request.method === "GET"){
-        getAllJokes(request, response);
+    if(request.url === "/api/jokes" && request.method === "GET"){
+        let allJokes = getAllJokes(request, response);
+        response.writeHead(200, {"Content-type":"text/json"})
+        response.end(JSON.stringify(allJokes));
     }
 })
 
 server.listen(3000);
 
 function getAllJokes(request, response){
-
+    let arrayOfJokes = []
+    let pathToData = path.join(__dirname, "data");
+    let data = fs.readdirSync(pathToData);
+    for(let i = 0; i < data.length; i++){
+        let pathToFile = path.join(pathToData, `${i}.json`)
+        let jokeString = fs.readFileSync(pathToFile, "utf-8");
+        let joke = JSON.parse(jokeString);
+        arrayOfJokes.push(joke);
+    }
+    return arrayOfJokes;
 }
